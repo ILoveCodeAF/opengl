20bc4a0ce7e83bae962f57d2bfc85cda0fb08b13
hieule <leconghieu1998@gmail.com>
2020-02-08 17:00:23 +0700

    draw triagle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include "shader.h"
+
 int main(){
 	glewExperimental = true;
 	if( !glfwInit() ){
@@ -16,7 +18,7 @@ int main(){
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	GLFWwindow* window;
-	window = glfwCreateWindow(1023, 768, "turorial", NULL, NULL);
+	window = glfwCreateWindow(640, 480, "turorial", NULL, NULL);
 	if( window == NULL ){
 		std::cout << "Failed to open GLFW window, if you have an Intel GPU they are not 3.3 compatible. try another version" << std::endl;
 		glfwTerminate();
@@ -32,8 +34,41 @@ int main(){
 
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	GLuint vertexArrayID;
+	glGenVertexArrays(1, &vertexArrayID);
+	glBindVertexArray(vertexArrayID);
+
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+	};
+	GLuint vertexBuffer;
+	glGenBuffers(1, &vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint programID = loadShaders( "shader/vertex_shader.vertexshader", "shader/fragment_shader.fragmentshader");
+	
 	do{
-		glClear( GL_COLOR_BUFFER_BIT );
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+		glUseProgram(programID);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+		glVertexAttribPointer(
+				0,
+				3,
+				GL_FLOAT,
+				GL_FALSE,
+				0,
+				(void*)0
+		);
+		
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDisableVertexAttribArray(0);
+
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
---
+++ shader.h
@@ -0,0 +1,8 @@
+#ifndef __SHADER_H_INCLUDED__
+#define __SHADER_H_INCLUDED__
+
+#include <GL/glew.h>
+
+GLuint loadShaders(const char *vertex_file_path, const char *fragment_file_path);
+
+#endif
